4a299eb734fc3d46fad831e69987bc34f2b453a5
Vladimir Shits <rav9aa@gmail.com>
2022-04-03 17:40:08 +0300

    first endpoint created

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const hostname = '127.0.0.1'
 const port = 4000;
 
 const server = http.createServer((req, res)=>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World')
+
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	if ( req.method === 'OPTIONS' ) {
+		res.writeHead(200);
+		res.end();
+		return;
+	}
+
+    if(req.url === '/users'){
+        res.write(`[{"name": "Vova"}]`)
+    }
+    res.end()
 })
 
 server.listen(port, hostname, ()=>{
