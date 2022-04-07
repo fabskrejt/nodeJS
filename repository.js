6ac97f1bd7c1a90601793c49a2685c842a032dfa
Vladimir Shits <rav9aa@gmail.com>
2022-04-07 23:20:09 +0300

    apply promisification

--- repository.js
+++ repository.js
@@ -2,21 +2,24 @@ const fs = require('fs')
 
 //const users = [{ name: 'User1' }, { name: 'User2' }, { name: 'User3' },]
 
-const getUsers = (callback) => {
+const getUsers = () => {
 
-fs.readFile('users.json', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log(data)
-  callback(data)
-  //return data
-})
+  return new Promise((res, rej) => {
+    fs.readFile('users.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(data)
+      res(data)
+      //callback(data)
+    })
+  })
+
 }
 
 const addUser = (name) => {
-    users.push({name})
+  users.push({ name })
 }
 
 exports.getUsers = getUsers
--- usersController.js
+++ usersController.js
@@ -1,15 +1,13 @@
 const { getUsers, addUser } = require('./repository');
 
-exports.usersController = (req, res) => {
+exports.usersController = async (req, res) => {
     if (req.method === 'POST') {
         addUser('User4')
         res.write(JSON.stringify({ success: true }))
         res.end()
     } else {
-        getUsers((users) => {
-            res.write(users)
-            res.end()
-        })
-        // res.write(JSON.stringify(getUsers()))
+        const users = await getUsers()
+        res.write(users)
+        res.end()
     }
 }
